8425f289b51d627f886ec40208a25d3e625cc04f
Camilo Saavedra <ci.saavedra@gmail.com>
2024-07-10 13:07:59 -0400

    | ADD | Add react-router-dom for routing and createBrowserRouter for routing configuration

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,29 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import TableContent from "./components/TableContent.tsx";
 
 const client = new ApolloClient({
-  uri: "https://odyssey-lift-off-server.herokuapp.com/",
+  uri: "https://rickandmortyapi.com/graphql",
   cache: new InMemoryCache(),
 });
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/episode/:ids",
+    element: <TableContent />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <RouterProvider router={router} />
     </ApolloProvider>
   </React.StrictMode>,
 );
