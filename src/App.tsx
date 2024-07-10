f50b7af58f309838d73d3295087543a810cd5a70
Camilo Saavedra <ci.saavedra@gmail.com>
2024-07-10 09:15:11 -0400

    | ADD | Add Prettier configuration and Apollo dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-
 function App() {
-
   return (
-    <div className="w-full h-screen bg-black text-white">
+    <div className="h-screen w-full bg-black text-white">
       <h1>Hello</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
