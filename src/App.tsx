5ec1df1cb52e61fb910253cd5844ade2b3174291
Camilo Saavedra <ci.saavedra@gmail.com>
2024-07-10 09:18:42 -0400

    | ADD |  Add Flowbite React library and update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "flowbite-react";
+
 function App() {
   return (
     <div className="h-screen w-full bg-black text-white">
       <h1>Hello</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
